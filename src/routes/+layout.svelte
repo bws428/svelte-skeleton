8e147c95a65330773ae3612889f100b18a4ac78c
Brian D. Wendt <briandwendt@gmail.com>
2023-10-29 15:44:20 -0400

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import Nav from './nav.svelte';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header"><Nav /></svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts"></script>
+
+<!-- <div class="container h-full mx-auto flex justify-center">
+	<div class="space-y-10 text-center flex flex-col py-40">
+		<h2 class="h2">Welcome to Skeleton.</h2>
+		<div class="flex justify-center space-x-2">
+			<a class="btn variant-filled" href="/"> Get started for free </a>
+		</div>
+		<div class="space-y-2">
+			<p>Try editing the following:</p>
+			<p><code class="code">/src/routes/+layout.svelte</code></p>
+			<p><code class="code">/src/routes/+page.svelte</code></p>
+		</div>
+	</div>
+</div> -->
+<!-- END SKeleton stuff
+-->
+
+<div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80" />
+<div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-40 text-center">
+	<!-- Product Tagline -->
+	<h1 class="text-4xl font-bold tracking-tight sm:text-6xl">
+		Streamline your aviation record keeping
+	</h1>
+	<!-- Product Description -->
+	<p class="mt-6 text-lg leading-8">
+		Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit
+		sunt amet fugiat veniam occaecat fugiat aliqua.
+	</p>
+	<!-- CTA Buttons -->
+	<div class="justify-center space-x-2 space-y-10">
+		<a class="btn variant-filled px-12" href="/">
+			Get started for free <span aria-hidden="true" class="ml-2"> → </span></a
+		>
+		<a href="/auth/login" class="btn variant-soft-surface text-sm leading-6 px-8">Learn more </a>
+	</div>
+</div>
